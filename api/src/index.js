e1b132bdf535a33f5ab0e69d22b141bc36ac9f08
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-12-30 19:45:48 +0100

    Initialize Express server with a simple 'hello world' endpoint

---
+++ api/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+/**
+ * GET endpoint, providing hello world
+ *
+ * @param
+ * @returns
+ */
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello world" });
+});
+
+app.listen(3000, (err) => {
+  if (!err) {
+    console.log("running on port " + 3000);
+  } else {
+    console.log(err);
+  }
+});
