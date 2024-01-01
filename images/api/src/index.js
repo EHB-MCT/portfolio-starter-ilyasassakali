3d1e437744fee6dea5c4ed9e7f0ef6d88d478227
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2024-01-01 01:46:42 +0100

    Add Games Table and User Table with Knexjs Migration

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -1,20 +1,42 @@
 const express = require("express");
+const knex = require("knex");
+const knexfile = require("../knexfile");
 const app = express();
+const db = knex(knexfile.development);
+app.use(express.json());
+
+const User = require("../classes/User");
 
 /**
- * GET endpoint, providing hello world
+ * Root endpoint for a simple hello world message.
  *
- * @param
- * @returns
+ * @returns {object} - Hello world message.
  */
-
 app.get("/", (req, res) => {
-  res.send({ message: "hi world" });
+  res.send({ message: "Hello, world!" });
+});
+
+/**
+ * Endpoint to retrieve the list of users.
+ *
+ * @returns {object} - List of users.
+ */
+app.get("/users", async (req, res) => {
+  try {
+    const usersData = await db("users").select("*");
+    const users = usersData.map(
+      (user) => new User(user.username, user.email, user.password)
+    );
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(3000, (err) => {
   if (!err) {
-    console.log("running on port " + 3000);
+    console.log("running on port" + 3000);
   } else {
     console.log(err);
   }
